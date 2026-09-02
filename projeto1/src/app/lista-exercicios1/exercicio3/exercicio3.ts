ed64221eae75626db77cb7ea4378dcb8a825a782
MiguelPrevedeli <projetointegradorunifio2026@gmail.com>
2026-09-02 01:42:27 +0000

    exercicio3

--- projeto1/src/app/lista-exercicios1/exercicio3/exercicio3.ts
+++ projeto1/src/app/lista-exercicios1/exercicio3/exercicio3.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-exercicio3',
   standalone: false,
   templateUrl: './exercicio3.html',
   styleUrl: './exercicio3.css',
 })
-export class Exercicio3 {}
+export class Exercicio3 {
+  imagemUrl = 'https://angular.io/assets/images/logos/angular/angular.svg';
+
+
+}
